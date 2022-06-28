a27d0baaf61699aaae79da635b6cb3eb4cb40a89
Amirhossein Alibakhshi <amirhosseinalibakhshi@gmail.com>
2022-06-28 14:53:07 +0430

    ping

--- agent/agent.ino
+++ agent/agent.ino
@@ -1,73 +1,140 @@
 #include <ESP8266WiFi.h>
-#include <PubSubClient.h>
+#include <WiFiClient.h>
+#include <ArduinoMqttClient.h>
+#include <NTPClient.h>
+#include <WiFiUdp.h>
+#include <ArduinoJson.h>
 
-// GPIO 5 D1
-#define LED 5
+#define LDR_PIN A0
+int LEDS_PIN[] = {D1, D2, D3, D4};
 
-// WiFi
-const char *ssid = "Dellink"; // Enter your WiFi name
-const char *password = "wtkh-daah-y8bj";  // Enter WiFi password
+String ssid = "Dellink";
+String pass = "wtkh-daah-y8bj";
 
-// MQTT Broker
-const char *mqtt_broker = "broker.emqx.io";
-const char *topic = "esp8266/led";
-const char *mqtt_username = "emqx";
-const char *mqtt_password = "public";
-const int mqtt_port = 1883;
+WiFiClient wifiClient;
+MqttClient mqttClient(wifiClient);
 
-WiFiClient espClient;
-PubSubClient client(espClient);
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, "pool.ntp.org");
+
+const char broker[] = "192.168.1.7";
+int        port     = 1883;
+String ping_topic = "I1820/main/agent/ping";
+String log_topic = "I1820/main/log/send";
+String notif_topic = "I1820/main/configuration/request";
+
+const long ping_interval = 10000;
+const long log_interval = 5000;
+
+unsigned long previous_ping_millis = 0;
+unsigned long previous_log_millis = 0;
+
+StaticJsonDocument<200> doc;
+
+void onMqttMessage(int messageSize) {
+  // we received a message, print out the topic and contents
+  Serial.println("Received a message with topic '");
+  Serial.print(mqttClient.messageTopic());
+  Serial.print("', length ");
+  Serial.print(messageSize);
+  Serial.println(" bytes:");
+
+  String json = "";
+
+  // use the Stream interface to print the contents
+  while (mqttClient.available())
+    json += (char)mqttClient.read();
+
+  Serial.println(json);
+  deserializeJson(doc, json);
+  //if ((doc["agent"] == "nodemcu") && (doc["settings"][0]["name"] == "on")) {
+  if (doc["agent"] == "nodemcu") {
+    if (doc["device"] == "l1")
+      digitalWrite(LEDS_PIN[0], doc["settings"][0]["value"]);
+    else if (doc["device"] == "l2")
+      digitalWrite(LEDS_PIN[1], doc["settings"][0]["value"]);
+    else if (doc["device"] == "l3")
+      digitalWrite(LEDS_PIN[2], doc["settings"][0]["value"]);
+    else if (doc["device"] == "l4")
+      digitalWrite(LEDS_PIN[3], doc["settings"][0]["value"]);
+  }
+  Serial.println();
+}
+
+void send_ping() {
+  Serial.println("----------------------------------------------------------------------------------------------");
+  Serial.print("[");
+  Serial.print(timeClient.getFormattedTime());
+  Serial.print("] [PING] (");
+  Serial.print(ping_topic);
+  Serial.println(")");
+  String ping = "{\"id\":\"nodemcu\",\"things\":[{\"id\":\"ldr\",\"type\":\"light\"},{\"id\":\"l1\",\"type\":\"lamp\"},{\"id\":\"l2\",\"type\":\"lamp\"},{\"id\":\"l3\",\"type\":\"lamp\"},{\"id\":\"l4\",\"type\":\"lamp\"}],\"actions\":[]}";
+  Serial.println(ping);
+  mqttClient.beginMessage(ping_topic);
+  mqttClient.print(ping);
+  mqttClient.endMessage();
+}
+
+void send_log() {
+  Serial.println("----------------------------------------------------------------------------------------------");
+  Serial.print("[");
+  Serial.print(timeClient.getFormattedTime());
+  Serial.print("] [LOG] (");
+  Serial.print(log_topic);
+  Serial.println(")");
+  int ldr_value = analogRead(LDR_PIN);
+  String ldr_message = "{\"timestamp\":" + String(timeClient.getEpochTime()) + ",\"type\":\"light\",\"device\":\"ldr\",\"states\":[{\"name\":\"light\",\"value\":" + String(map(ldr_value, 0, 1024, 100, 0)) + "}],\"agent\":\"nodemcu\"}";
+  Serial.println(ldr_message);
+  mqttClient.beginMessage(log_topic);
+  mqttClient.print(ldr_message);
+  mqttClient.endMessage();
+}
+
 
 void setup() {
-  // Set software serial baud to 115200;
-  Serial.begin(115200);
-  // connecting to a WiFi network
-  WiFi.begin(ssid, password);
+  //Initialize serial and wait for port to open:
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+  Serial.print("Attempting to connect to WPA SSID: ");
+  Serial.print(ssid);
+  WiFi.begin(ssid, pass);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.println("Connecting to WiFi..");
   }
   Serial.println("Connected to the WiFi network");
-  //connecting to a mqtt broker
-  client.setServer(mqtt_broker, mqtt_port);
-  client.setCallback(callback);
-  while (!client.connected()) {
-    String client_id = "esp8266-client-";
-    client_id += String(WiFi.macAddress());
-    Serial.println("Connecting to public emqx mqtt broker.....");
-    //        if (client.connect(client_id, mqtt_username, mqtt_password)) {
-    if (client.connect(client_id.c_str())) {
-      Serial.println("Public emqx mqtt broker connected");
-    } else {
-      Serial.print("failed with state ");
-      Serial.print(client.state());
-      delay(2000);
-    }
-  }
-  // publish and subscribe
-  client.publish(topic, "hello emqx");
-  client.subscribe(topic);
-}
-
-void callback(char *topic, byte *payload, unsigned int length) {
-  Serial.print("Message arrived in topic: ");
-  Serial.println(topic);
-  Serial.print("Message:");
-  String message;
-  for (int i = 0; i < length; i++) {
-    message = message + (char) payload[i];  // convert *byte to string
-  }
-  Serial.print(message);
-  if (message == "on") {
-    digitalWrite(LED, LOW);  // LED on
-  }
-  if (message == "off") {
-    digitalWrite(LED, HIGH);  // LED off
-  }
   Serial.println();
-  Serial.println("-----------------------");
+  Serial.print("Attempting to connect to the MQTT broker: ");
+  Serial.println(broker);
+  if (!mqttClient.connect(broker, port)) {
+    Serial.print("MQTT connection failed! Error code = ");
+    Serial.println(mqttClient.connectError());
+    while (1);
+  }
+  Serial.println("You're connected to the MQTT broker!");
+  Serial.println();
+  mqttClient.onMessage(onMqttMessage);
+  mqttClient.subscribe(notif_topic);
+  for (unsigned int i = 0; i < sizeof(LEDS_PIN) / sizeof(LEDS_PIN[0]); i++)
+    pinMode(LEDS_PIN[i], OUTPUT);
+  timeClient.begin();
+  timeClient.setTimeOffset(16200); // GMT+4:30 (4.5*3600)
 }
 
 void loop() {
-  client.loop();
+  mqttClient.poll();
+  timeClient.update();
+  unsigned long currentMillis = millis();
+  
+  if (currentMillis - previous_ping_millis >= ping_interval) {
+    previous_ping_millis = currentMillis;
+    send_ping();
+  }
+  
+  if (currentMillis - previous_log_millis >= log_interval) {
+    previous_log_millis = currentMillis;
+    send_log();
+  }
 }
